057c9adfc87e718b53c9edb46a19008faac8d6b5
Ichongiri, Chie <cichongiri@deloitte.co.uk>
2024-02-04 14:30:31 +0000

    wip: Init baseline for 'receptionist' and 'concierge' functions

--- src/index.js
+++ src/index.js
@@ -15,6 +15,28 @@ app.get("/", (req, res) => {
     res.send('<h1>The data endpoint is at "<code>/items</code>"</h1>')
 })
 
+function receptionist(request, response) {
+    const sanitizedRequest = Object.freeze({
+        path: request.path,
+        method: request.method,
+        pathParams: request.params,
+        queryParams: request.query,
+        body: request.body,
+    });
+
+    concierge(sanitizedRequest)
+        .then(({headers, statusCode, data}) => response.set(headers).status(statusCode).send(data))
+        .catch(e => response.status(500).end())
+}
+
+function concierge(request) {
+    return new Promise((resolve, reject) => resolve({
+        headers: {},
+        statusCode: 200,
+        data: {}
+    }))
+}
+
 db.init().then(() => {
     app.listen(3000, () => console.log('Listening on port 3000'));
 }).catch((err) => {
